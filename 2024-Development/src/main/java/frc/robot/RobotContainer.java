73590e7c6bc37058b939866a47750380b57607b8
BLUEARROW7000 <93559710+BLUEARROW7000@users.noreply.github.com>
2024-01-20 15:37:46 -0500

    Path Planner V.1
    - added the autobuilder configurator
    - made a new path/auto (I hate big chungus)

--- 2024-Development/src/main/java/frc/robot/RobotContainer.java
+++ 2024-Development/src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,11 @@ import frc.robot.lib.LimeLight;
 
 import frc.robot.lib.Constants.OIConstants;
 import frc.robot.lib.Constants.POIGeometryConstants;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -23,6 +28,9 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
+
+  private final SendableChooser<Command> autoChooser;
+
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final SwerveDrive SUBSYSTEM_SWERVEDRIVE = new SwerveDrive();
@@ -79,9 +87,18 @@ public class RobotContainer {
         () -> JOYSTICK_DRIVER.getRawAxis(OIConstants.CONTROLLER_DRIVER_Y), 
         () -> JOYSTICK_DRIVER.getRawAxis(OIConstants.CONTROLLER_DRIVER_Z), 
         () -> true)
+        
     );
     // Configure the trigger bindings
     configureBindings();
+
+    // Build an auto chooser. This will use Commands.none() as the default option.
+    autoChooser = AutoBuilder.buildAutoChooser("I hate big chungus");
+
+        // Another option that allows you to specify the default auto by its name
+        // autoChooser = AutoBuilder.buildAutoChooser("My Default Auto");
+
+    SmartDashboard.putData("Auto Chooser", autoChooser);
   }
 
   private void configureBindings() {
@@ -115,6 +132,10 @@ public class RobotContainer {
 
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return  new PathPlannerAuto("I hate big chungus");
   }
+
+  // public Command getAutonomousCommand() {
+  //   return autoChooser.getSelected();
+  // } 
 }
--- 2024-Development/src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ 2024-Development/src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -1,6 +1,10 @@
 package frc.robot.subsystems;
 //kauai
 import com.kauailabs.navx.frc.AHRS;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
@@ -21,6 +25,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.lib.Constants;
 import frc.robot.lib.SwerveModule;
@@ -95,6 +100,33 @@ public class SwerveDrive extends SubsystemBase {
         MODULE_BACK_LEFT.resetEncoders();
         MODULE_BACK_RIGHT.resetEncoders();
 
+        // Configure AutoBuilder last
+        AutoBuilder.configureHolonomic(
+                this::getPose, // Robot pose supplier
+                this::resetOdometer, // Method to reset odometry (will be called if your auto has a starting pose)
+                this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+                this::setChassisSpeed, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+                new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
+                        new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+                        new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
+                        4.5, // Max module speed, in m/s
+                        0.4, // Drive base radius in meters. Distance from robot center to furthest module.
+                        new ReplanningConfig() // Default path replanning config. See the API for the options here
+                ),
+                () -> {
+                    // Boolean supplier that controls when the path will be mirrored for the red alliance
+                    // This will flip the path being followed to the red side of the field.
+                    // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+                    var alliance = DriverStation.getAlliance();
+                    if (alliance.isPresent()) {
+                        return alliance.get() == DriverStation.Alliance.Red;
+                    }
+                    return false;
+                },
+                this // Reference to this subsystem to set requirements
+        );
+
     }
 
     @Override
@@ -120,6 +152,15 @@ public class SwerveDrive extends SubsystemBase {
 
         SmartDashboard.putString("Robot Odemeter position", ODEMETER.getPoseMeters().toString());
 
+    }
+
+    public Pose2d getPose() {
+        Pose2d currentPos = ODEMETER.getPoseMeters();
+        return currentPos;
+    }
+
+    public ChassisSpeeds getRobotRelativeSpeeds() {
+        return ChassisSpeeds.fromFieldRelativeSpeeds(null, null);
     }
 
     public double getRobotHeading() {
@@ -232,5 +273,10 @@ public class SwerveDrive extends SubsystemBase {
     public Command zeroRoboOdemetry() {
         return Commands.runOnce(()-> zeroModules());
     }
+
+    // public Pose2d getPose() {
+    //     return Pose2d(x, y, Rotation2d.fromDegrees(deg));
+    // }
+
 
 }
