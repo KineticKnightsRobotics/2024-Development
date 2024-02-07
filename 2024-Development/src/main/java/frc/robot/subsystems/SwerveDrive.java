7cff5152494fe7af8966b208b46293c37fbc6845
Danny B <61749098+GameBox420@users.noreply.github.com>
2024-02-06 19:39:27 -0500

    idk

--- 2024-Development/src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ 2024-Development/src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -111,7 +111,9 @@ public class SwerveDrive extends SubsystemBase {
                         new PIDConstants(TrajectoryTurning.Proportional,TrajectoryTurning.Integral,TrajectoryTurning.Derivitive), // Rotation PID constants
                         0.05, // Max module speed, in m/s
                         KinematicsConstants.RADIUS_DRIVE_CHASSIS, // Drive base radius in meters. Distance from robot center to furthest module.
-                        new ReplanningConfig() // Default path replanning config. See the API for the options here
+                        new ReplanningConfig(
+                            
+                        ) // Default path replanning config. See the API for the options here
                 ),
                 () -> {
                     // Boolean supplier that controls when the path will be mirrored for the red alliance
