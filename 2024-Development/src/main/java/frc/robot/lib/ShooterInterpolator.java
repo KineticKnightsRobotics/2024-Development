fde167e814bc48854e32c54dfbb6320eb8562fe0
Danny B <61749098+GameBox420@users.noreply.github.com>
2024-03-28 10:38:31 -0400

    New Regression function

--- 2024-Development/src/main/java/frc/robot/lib/ShooterInterpolator.java
+++ 2024-Development/src/main/java/frc/robot/lib/ShooterInterpolator.java
@@ -43,30 +43,44 @@ public class ShooterInterpolator {
     }
 
     public double getTilterAimAngle(double distance) {
-        double angle = 
-        //y= -9.5824x^{3}+55.2489x^{2}-79.7603x+45.3198
+        if (distance < 1.35 || 3.28 < distance) {
+            return 0.0;
+        }
+        else {
+            return 
+            +1.9043     *(Math.pow(distance,3))
+            -19.4112    *(Math.pow(distance,2))
+            +69.6266    *distance
+            -55.5631
+            ;
+        }
 
         //polynomial regression...? ...sigma...
 
+        //Using first and second set of data
+        /*
+         * y=1.9043x^3−19.4112x^2+69.6255x−55.5631
+         * +1.9043    *Math.pow(distance,3)
+         * -19.4112   *Math.pow(distance,2)
+         * +69,6266   *distance
+         * -55.5631
+         */
+        
 
+        /* Only first set of data
+        -6.7104*(Math.pow(distance, 2))
+        +43.5977*distance
+        -40.5278;
+        */
+
+
+        /* Equation pre-North Bay
         -9.5824*(Math.pow(distance,3))
         +55.2489*(Math.pow(distance,2))
         -79.7603*distance
         +45.3198;
-
-        /* 
-        (1*(Math.pow(distance,4))) +
-        (1*(Math.pow(distance,3))) +
-        (1*(Math.pow(distance,2))) + 
-        (1*(Math.pow(distance,1)));
         */
 
-        if (distance < 3.6) {
-            return angle;
-        }
-        else {
-            return 0.0;
-        }
 
     }
 
