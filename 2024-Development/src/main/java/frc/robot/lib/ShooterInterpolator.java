b4ae8e1add3e1f52f8a0bd84e26af496cc2d5bfe
Danny B <61749098+GameBox420@users.noreply.github.com>
2024-03-28 10:42:29 -0400

    Remove Unused code in Shooter Interpolator
    Gotta get that memory back somehow

--- 2024-Development/src/main/java/frc/robot/lib/ShooterInterpolator.java
+++ 2024-Development/src/main/java/frc/robot/lib/ShooterInterpolator.java
@@ -5,52 +5,15 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ShooterInterpolator {
 
-
-    public double data[][] = {
-    };
-
-    InterpolatingDoubleTreeMap m_InterpolatingDoubleTreeMap = new InterpolatingDoubleTreeMap();
-
-
-    //PolynomialRegression m_tiltPolynomialRegression
-
-    public ShooterInterpolator() {
-        m_InterpolatingDoubleTreeMap.put(1.305,0.0);
-        m_InterpolatingDoubleTreeMap.put(1.47,10.0);
-        m_InterpolatingDoubleTreeMap.put(2.085,20.0);
-        m_InterpolatingDoubleTreeMap.put(2.65,30.0);
-        m_InterpolatingDoubleTreeMap.put(3.405, 30.5);
-
-        for (int x = 0; x < data.length; x++) {
-            m_InterpolatingDoubleTreeMap.put(data[x][0],data[x][1]);
-        }
-        //m_tiltPolynomialRegression = new PolynomialRegression(
-        //    Array.listOf, 0);
-    }
-
-    public double interpolateAngle(double distance) {
-        SmartDashboard.putNumber("Distance to Speaker", distance);
-        SmartDashboard.putNumber("Interpolated Angle", m_InterpolatingDoubleTreeMap.get(distance));
-        return m_InterpolatingDoubleTreeMap.get(distance);
-    }
-
-    public double findAngleRegression(double distance) {
-        return 0.0;
-    }
-
-    public double returnAngleEquation(double distance) {
-        return 0.0;
-    }
-
     public double getTilterAimAngle(double distance) {
         if (distance < 1.35 || 3.28 < distance) {
             return 0.0;
         }
         else {
             return 
-            +1.9043     *(Math.pow(distance,3))
-            -19.4112    *(Math.pow(distance,2))
-            +69.6266    *distance
+            +1.9043   *(Math.pow(distance,3))
+            -19.4112  *(Math.pow(distance,2))
+            +69.6266  *distance
             -55.5631
             ;
         }
@@ -66,13 +29,11 @@ public class ShooterInterpolator {
          * -55.5631
          */
         
-
         /* Only first set of data
         -6.7104*(Math.pow(distance, 2))
         +43.5977*distance
         -40.5278;
         */
-
 
         /* Equation pre-North Bay
         -9.5824*(Math.pow(distance,3))
@@ -80,8 +41,6 @@ public class ShooterInterpolator {
         -79.7603*distance
         +45.3198;
         */
-
-
     }
 
 }
